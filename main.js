ad878c09b28f0c52bb41520e075334d9c3a918b6
mariany-silva <129884463+mariany-silva@users.noreply.github.com>
2024-04-09 15:09:39 -0300

    s

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const botoes = document.querySelectorA11(".botao");
-console.log(botoes);
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclik = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classlist.remove("ativo");
+        }
+        
+        botoes[i].classlist.add("ativo");
+    }
+    console.log(i);
+}
+    
