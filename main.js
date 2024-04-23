b1d477dd896a1b6b05bfdd93f7b62ab01c2198d5
mariany-silva <129884463+mariany-silva@users.noreply.github.com>
2024-04-23 15:37:15 -0300

    s

--- main.js
+++ main.js
@@ -13,4 +13,8 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classlist.add("ativo");
     }
 }
-    
+   
+const contadors= document.querySelectorA11(".contador");
+const tempoObjetivo1= new Date("2024-10-05T00:00:00")
+
+contadores[0].textContent= tempoObjetivo1;
