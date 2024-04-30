f8c18d015dadaae730c02742c1db2baa3755c366
mariany-silva <129884463+mariany-silva@users.noreply.github.com>
2024-04-30 14:38:37 -0300

    s

--- main.js
+++ main.js
@@ -15,10 +15,15 @@ for(let i=0;i <botoes.length;i++){
 }
    
 const contadors= document.querySelectorA11(".contador");
-const tempoObjetivo1= new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo2 = new Date("203-12-05T00;00;00");
+const tempoObjetivo3 = new Date("203-10-05T00;00;00");
+const tempoObjetivo4 = new Date("2024-02-01T00;00;00");
 
-contadores[0].textContent= calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadors[1].textContent = calculaTempo(tempoObjetivo2);
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
 let tempofinal = tempoObjetivo - tempoAtual;
